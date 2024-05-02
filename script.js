1bf1576b0ce9b7b97982064e4c9aec9bbff47872
Roman <pomincukr@gmail.com>
2024-05-02 16:46:32 +0500

    final edition

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function solution(str) {
-  let pattern = /\w{2}/g
-  let finding = str.match(pattern)
-  return finding
+  let pattern = /\w{2}/g;
+  let finding = str.match(pattern);
+  if (!finding) {
+    return [];
+  }
+  if (str.length % 2 !== 0) {
+    finding.push(str[str.length - 1] + "_");
+  }
+  return finding;
 }
 
-console.log(solution("abcdef"))
+console.log(solution("jds"));
